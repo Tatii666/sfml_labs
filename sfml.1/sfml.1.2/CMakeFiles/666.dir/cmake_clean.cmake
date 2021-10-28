27fcb55dc9fead46ffec22efedde8945a6d027e2
Tatii666 <67223121+Tatii666@users.noreply.github.com>
2021-10-28 20:25:40 +0300

    Add files via upload

---
+++ sfml.1/sfml.1.2/CMakeFiles/666.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/666.dir/main.cpp.obj"
+  "666.pdb"
+  "666.exe"
+  "666.exe.manifest"
+  "lib666.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/666.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ sfml.1/sfml.1.2/CMakeFiles/sfml.1.2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfml.1.2.dir/main.cpp.obj"
+  "sfml.1.2.pdb"
+  "sfml.1.2.exe"
+  "sfml.1.2.exe.manifest"
+  "libsfml.1.2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfml.1.2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
