38be003e35bc7c230743e885e184273ac6eaba48
Tatii666 <67223121+Tatii666@users.noreply.github.com>
2021-10-28 20:38:41 +0300

    Add files via upload

---
+++ sfml.3/sfml3.1/CMakeFiles/03.1.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/03.1.dir/main.cpp.obj"
+  "03.1.pdb"
+  "03.1.exe"
+  "03.1.exe.manifest"
+  "lib03.1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/03.1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
