ef2f52b10c7e576c8f90c8e2864c220d1c2456a1
Tatii666 <67223121+Tatii666@users.noreply.github.com>
2021-10-28 20:40:12 +0300

    Add files via upload

---
+++ sfml.3/Sfml3.3/CMakeFiles/sfml3.3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sfml3.3.dir/main.cpp.obj"
+  "sfml3.3.pdb"
+  "sfml3.3.exe"
+  "sfml3.3.exe.manifest"
+  "libsfml3.3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sfml3.3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
